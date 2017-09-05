762d29cd569ad92379b9e7ebab2a1a8779dadd8b
Aaron Buchanan <aaron.buchanan@hyfn.com>
2017-09-05 15:30:42 -0700

    wip #1

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+// Keeps track of the length of the 'games'
+exports.countGames = functions.database.ref('/games/{date}/{gameid}').onCreate(event => {
+  const collectionRef = event.data.ref.parent;
+  const countRef = functions.database.ref('/stats').child('total_games');
+
+  return countRef.transaction(current => {
+    if (event.data.exists() && !event.data.previous.exists()) {
+      return (current || 0) + 1;
+    }
+    else if (!event.data.exists() && event.data.previous.exists()) {
+      return (current || 0) - 1;
+    }
+  }).then(() => {
+    console.log('Counter updated.');
+  });
+});
+
+exports.recountGames = functions.database.ref('/stats/total_games').onWrite(event => {
+  if (!event.data.exists()) {
+    const counterRef = event.data.ref;
+    const collectionRef = functions.database.ref('/games/{date}');
+
+    return collectionRef.once('value')
+        .then(gamesData => counterRef.set(gamesData.numChildren()));
+  }
+});
